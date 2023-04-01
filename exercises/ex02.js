5d2f038088d2f860f6c81fae10e9fd6baf815537
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-04-01 11:43:44 -0700

    Solve exercise 2

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -8,6 +8,21 @@ Create a function named conditionalSum that will be given an array of numbers an
 
 const conditionalSum = function (values, condition) {
   // Your code here
+  const odd = function(num) { if(num % 2 == 1) return true }
+  const even = function(num) { if(num % 2 == 0) return true }
+
+  // Update the values by filtering
+  if(condition === "odd") values = values.filter(odd)
+  else if(condition === "even") values = values.filter(even)
+  
+  if(values.length > 0) {
+    const sum = values.reduce(function(accumulator, currentValue) {
+      return accumulator + currentValue
+    })
+    return sum
+  } else {
+    return 0
+  }
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
