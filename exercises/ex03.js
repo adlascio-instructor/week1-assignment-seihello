16915e8072edb19f56c162b33dd7ffc69ff70c69
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-04-01 11:49:11 -0700

    Solve exercise 3

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -9,6 +9,13 @@ Create a function named numberOfVowels that will receive a string and return the
 
 const numberOfVowels = function (data) {
   // Put your solution here
+  array = [...data]
+  vowels = array.filter(function(chara) {
+    if(chara == 'a' || chara == 'e' || chara == 'i' || chara == 'o' || chara == 'u') {
+      return true
+    }
+  })
+  return vowels.length
 };
 
 console.log(numberOfVowels("orange")); // 3
